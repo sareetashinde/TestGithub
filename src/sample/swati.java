eab59c1a752f01a62d71b0b6f28e4f25710d2597
sareetashinde <88264909+sareetashinde@users.noreply.github.com>
2021-08-01 23:46:04 +0530

    Update swati.java

--- src/sample/swati.java
+++ src/sample/swati.java
@@ -6,6 +6,8 @@ public static void main(String[]args)
 {
 	
 System.out.println("-----happy diwali----");
+System.out.println("-----happy diwali----");
+System.out.println("-----happy diwali----");
 
 }
 
