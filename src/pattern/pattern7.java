2c67ed2334ded76065e0905d7314ce6eb9cd3e43
swati <swati@swati-PC>
2021-08-01 14:43:40 +0530

    pattern 7 made 16

--- src/pattern/pattern7.java
+++ src/pattern/pattern7.java
@@ -16,7 +16,7 @@ public class pattern7 {
 	public static void main(String[] args) {
 		
 		int star= 1;
-		for (int i=1;i<=10;i++)
+		for (int i=1;i<=15;i++)
 		{
 			for (int j=1;j<=star;j++)
 			{
