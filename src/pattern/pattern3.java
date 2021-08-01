d0a0821054489cd5824f8f7eeb6182f54d128f49
sareetashinde <88264909+sareetashinde@users.noreply.github.com>
2021-08-01 15:27:34 +0530

    Update pattern3.java

--- src/pattern/pattern3.java
+++ src/pattern/pattern3.java
@@ -13,7 +13,7 @@ public class pattern3 {
 	
 	public static void main(String[] args) {
 		
-		for (int i=1; i<=8; i++)
+		for (int i=1; i<=10;++)
 		{
 			for (int j=1; j<=5; j++) {
 				System.out.print("*  ");
