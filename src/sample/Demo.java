e34ebaf9e43c9798696548ed1294f6628970c052
swati <swati@swati-PC>
2021-08-01 14:48:42 +0530

    messsage print changed

--- src/sample/Demo.java
+++ src/sample/Demo.java
@@ -4,7 +4,7 @@ public class Demo
 {
 	public static void main(String[]args)
 	{
-		System.out.println("Happy frienship day...");
+		System.out.println("Happy frienship day all...all the best,....");
 	}
 
 }
